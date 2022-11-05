103099f1ee13d6bca9b005b3d6067c6c3a7e24cd
Thomas Ilk <winnie@thePoo.com>
2022-11-05 10:51:19 +0100

    add queue

--- Data.cpp
+++ Data.cpp
@@ -1,1 +1,27 @@
 #include "Data.h"
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+Data::Data(const std::string& value) : value(value)
+{
+  cout << "Data(" << value << ") constructed" << endl;
+}
+
+Data::~Data()
+{
+  cout << "Data(" << value << ") deconstructed" << endl;
+}
+
+Data* operator""_Data(const char* s, size_t)
+{
+  return new Data(s);
+}
+
+std::ostream& operator<<(std::ostream& os, const Data& d)
+{
+  // TODO: hier return-Anweisung eingeben
+  return os << '"' << d.value << '"' << std::flush;
+}
--- Data.h
+++ Data.h
@@ -5,5 +5,17 @@
 
 class Data
 {
+private:
+  std::string value;
+
+public:
+  Data(const std::string& value);
+  virtual ~Data();
+
+  // friend definiert Methode von auﬂerhalb der Klasse
+  friend std::ostream& operator<<(std::ostream& os, const Data& d);
 };
+
+//_Data --> User defined Literal
+Data* operator"" _Data(const char *s, size_t);
 
